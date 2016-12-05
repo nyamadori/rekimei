2784914ac0e7e13415fba55e9c29a7c8df746051
nyamadori <nyamadorig@gmail.com>
2016-12-05 22:24:55 +0900

    Add unique index for groups.slug

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161205130604) do
+ActiveRecord::Schema.define(version: 20161205132349) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "slug",       null: false
     t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["slug"], name: "index_groups_on_slug", unique: true, using: :btree
   end
 
 end
