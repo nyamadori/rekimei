dabbb3227a5ce0e71ea5685ef29d4543c7852ddd
nyamadori <nyamadorig@gmail.com>
2016-12-05 21:59:56 +0900

    Add routings related group and member

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :groups, path: '/', param: :slug do
+    resources :members do
+      resource :session, only: [:destroy, :new, :create]
+    end
+  end
 end
