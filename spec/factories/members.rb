84f883c778e887504093e6736b54532964e35533
nyamadori <nyamadorig@gmail.com>
2016-12-05 23:12:28 +0900

    Add member factory

--- spec/factories/members.rb
+++ spec/factories/members.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :member do
-    first_name "MyString"
-    last_name "MyString"
-    group nil
+    first_name { FFaker::Name.first_name }
+    last_name { FFaker::Name.last_name }
+    group
   end
 end
