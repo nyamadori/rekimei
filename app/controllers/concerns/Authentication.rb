e3f6fde9c12cc6b97ef86979efb58cfc245ab4ba
nyamadori <nyamadorig@gmail.com>
2016-12-09 14:32:57 +0900

    Add methods to retrieve current status

--- app/controllers/concerns/Authentication.rb
+++ app/controllers/concerns/Authentication.rb
@@ -4,5 +4,13 @@ concern :Authentication do
       session[:accounts] ||= {}
       session[:accounts][s.account.id] = true
     end
+
+    def current_accounts
+      session[:accounts].keys.map { |id| Account.find_by(id) }.compact
+    end
+
+    def current_groups
+      current_accounts.map(&:member).map(&:group)
+    end
   end
 end
