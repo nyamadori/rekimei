53846e1db882c898df12f6dac1d19db247b1b4f2
nyamadori <nyamadorig@gmail.com>
2016-12-09 13:47:19 +0900

    Add trait to populate accounts

--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -2,5 +2,13 @@ FactoryGirl.define do
   factory :group do
     name { FFaker::Company.name }
     slug { FFaker::Internet.slug }
+
+    trait :with_accounts do
+      after(:build) do |g|
+        g.members.build(account: build(:account))
+        g.members.build(account: build(:account))
+        g.members.build(account: build(:account))
+      end
+    end
   end
 end
