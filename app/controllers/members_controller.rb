1357fdce6ccbae9c529cba44695084f2e13316f4
nyamadori <nyamadorig@gmail.com>
2016-12-15 04:43:13 +0900

    Add accounts#edit and accounts#update

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,5 +1,27 @@
 class MembersController < ApplicationController
+  before_action :member, only: [:edit, :show, :destroy, :update]
+
   def index
     @members = current_group.members
+  end
+
+  def update
+    if member.update(member_params)
+      redirect_to action: :index
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def member
+    @member ||= Member.find(params[:id])
+  end
+
+  def member_params
+    params.require(:member).permit(
+      profile_attributes: [:id, :first_name, :last_name, :school_id]
+    )
   end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -2,4 +2,5 @@ class Member < ApplicationRecord
   belongs_to :account
   belongs_to :group
   has_one :profile
+  accepts_nested_attributes_for :profile
 end
