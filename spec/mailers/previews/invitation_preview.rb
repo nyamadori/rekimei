c2e2ab50007500737d8d97c4ab4ca9cd70d9e8df
nyamadori <nyamadorig@gmail.com>
2016-12-16 06:33:58 +0900

    Use factory girl to assign invitation

--- spec/mailers/previews/invitation_preview.rb
+++ spec/mailers/previews/invitation_preview.rb
@@ -1,9 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/invitation
 class InvitationPreview < ActionMailer::Preview
   def send_invitation
-    group = FactoryGirl.build(:group)
-    account = FactoryGirl.build(:account)
-    invitation = Invitation.new(group: group, account: account)
+    invitation = FactoryGirl.build(:invitation)
 
     InvitationMailer.send_invitation(invitation)
   end
