e03b54f948c8e21f0ad274089820f5a033fd4aae
nyamadori <nyamadorig@gmail.com>
2016-12-15 10:49:57 +0900

    Refactor

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -4,37 +4,29 @@ class Invitation
   attr_accessor :group, :account_attributes, :profile_attributes
 
   validates :group, presence: true
-  validate :validate_relations
+  validate :validate_models
 
   def save
     return unless valid?
-
-    @member = group.members.build(
-      account: account, profile: profile
-    )
-
-    @member.save
+    member.save
   end
 
   def account
-    @account ||= Account.new(account_attributes)
+    @account ||= member.build_account(account_attributes)
   end
 
   def profile
-    @profile ||= Profile.new(profile_attributes)
+    @profile ||= member.build_profile(profile_attributes)
   end
 
   def member
-    @member ||= group.members.build(account: account, profile: profile)
+    @member ||= group.members.build
   end
 
   private
 
-  def validate_relations
-    account.validate
-    profile.validate
-
-    errors[:account] << account.errors
-    errors[:profile] << profile.errors
+  def validate_models
+    errors.add(:account, :invalid) if account.invalid?
+    errors.add(:profile, :invalid) if profile.invalid?
   end
 end
