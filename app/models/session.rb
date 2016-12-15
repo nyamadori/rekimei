6444b652b58707e54a2c821e76f2e7436a2342c1
nyamadori <nyamadorig@gmail.com>
2016-12-16 08:32:53 +0900

    Shorten method body

--- app/models/session.rb
+++ app/models/session.rb
@@ -19,10 +19,7 @@ class Session
       @account = @group.accounts.find_by(email: email)
 
       unless @account&.authenticate(password)
-        errors.add(
-          :base, :email_or_password_invalid,
-          message: 'Invalid Email or Password'
-        )
+        errors.add(:base, :email_or_password_invalid, message: 'Invalid Email or Password')
       end
     else
       errors.add(:base, :no_group, message: "No group for #{group_slug}")
