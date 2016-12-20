07b8f0a0c0710cc10ba8649affdaeb6082b0da6c
nyamadori <nyamadorig@gmail.com>
2016-12-20 11:02:18 +0900

    Remove members routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   resource :session, only: [:destroy, :new, :create]
 
   resources :groups, path: '/', param: :slug, except: [:index] do
-    resources :members do
-      resource :account
-    end
+    resources :accounts
   end
 end
