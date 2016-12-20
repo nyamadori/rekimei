19e8cf85ffd06cca52f81c9f1a0bdd373a474448
nyamadori <nyamadorig@gmail.com>
2016-12-20 13:31:33 +0900

    Remove Member model

--- app/models/member.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-class Member < ApplicationRecord
-  belongs_to :account, required: false, autosave: true
-  belongs_to :group
-  has_one :profile, inverse_of: :member, dependent: :destroy, autosave: true
-  accepts_nested_attributes_for :profile
-end
